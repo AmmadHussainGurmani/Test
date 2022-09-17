f2d80c310c21e13c7d57f9f139cc776396bcc8da
Ammad <ammadhussain131@gmail.com>
2022-09-17 11:48:00 -0700

    about file

---
+++ About.vue
@@ -0,0 +1,41 @@
+<template>
+    <TopBar />
+    <section class="page-content section-spacing">
+		
+		<div class="container">
+			<h2>About Us</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, deleniti dolores quaerat maiores? Amet autem nisi quaerat ea animi a iste, veniam, asperiores dignissimos alias inventore voluptatibus laudantium consectetur velit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem esse, saepe, ipsam, quas praesentium accusamus sequi illo molestiae voluptates quam architecto ratione. At ratione ipsam recusandae saepe, qui eligendi, cum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid adipisci, eius iusto velit rem, hic aliquam natus, saepe reiciendis cupiditate soluta? Voluptatum, maiores? Vel, omnis. Temporibus distinctio iure, delectus error! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, deleniti dolores quaerat maiores? Amet autem nisi quaerat ea animi a iste, veniam, asperiores dignissimos alias inventore voluptatibus laudantium consectetur velit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem esse, saepe, ipsam, quas praesentium accusamus sequi illo molestiae voluptates quam architecto ratione. At ratione ipsam recusandae saepe, qui eligendi, cum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid adipisci, eius iusto velit rem, hic aliquam natus, saepe reiciendis cupiditate soluta? Voluptatum, maiores? Vel, omnis. Temporibus distinctio iure, delectus error! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, deleniti dolores quaerat maiores? Amet autem nisi quaerat ea animi a iste, veniam, asperiores dignissimos alias inventore voluptatibus laudantium consectetur velit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem esse, saepe, ipsam, quas praesentium accusamus sequi illo molestiae voluptates quam architecto ratione. At ratione ipsam recusandae saepe, qui eligendi, cum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid adipisci, eius iusto velit rem, hic aliquam natus, saepe reiciendis cupiditate soluta? Voluptatum, maiores? Vel, omnis. Temporibus distinctio iure, delectus error!</p>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, deleniti dolores quaerat maiores? Amet autem nisi quaerat ea animi a iste, veniam, asperiores dignissimos alias inventore voluptatibus laudantium consectetur velit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem esse, saepe, ipsam, quas praesentium accusamus sequi illo molestiae voluptates quam architecto ratione. At ratione ipsam recusandae saepe, qui eligendi, cum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid adipisci, eius iusto velit rem, hic aliquam natus, saepe reiciendis cupiditate soluta? Voluptatum, maiores? Vel, omnis. Temporibus distinctio iure, delectus error!</p>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, deleniti dolores quaerat maiores? Amet autem nisi quaerat ea animi a iste, veniam, asperiores dignissimos alias inventore voluptatibus laudantium consectetur velit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem esse, saepe, ipsam, quas praesentium accusamus sequi illo molestiae voluptates quam architecto ratione. At ratione ipsam recusandae saepe, qui eligendi, cum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid adipisci, eius iusto velit rem, hic aliquam natus, saepe reiciendis cupiditate soluta? Voluptatum, maiores? Vel, omnis. Temporibus distinctio iure, delectus error!</p>
+		</div>
+
+	</section>
+    <Contact />
+    <Footer />
+</template>
+
+<script>
+import TopBar from "./TopBar.vue";
+import Contact from "./Contact.vue";
+import Footer from "./Footer.vue";
+export default{
+    name : 'About',
+    components:{
+    TopBar,
+    Contact,
+    Footer
+}
+    
+}
+
+</script>
+
+<style>
+    .header .menu .main-menu a{
+        
+        
+    }
+
+</style>
